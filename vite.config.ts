7ed9a10f7dfe85430247dba6fa5a6527e124bf5a
Alex <git@pentive.com>
2023-01-26 10:50:54 -0600

    try to use static

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import solidStatic from "solid-start-static"
 
 export default defineConfig({
-  plugins: [solid({ ssr: false })],
+  plugins: [
+    solid({
+      ssr: true,
+      adapter: solidStatic(),
+    })
+  ],
   ssr: {
     noExternal: ["ag-grid-solid"]
   }
